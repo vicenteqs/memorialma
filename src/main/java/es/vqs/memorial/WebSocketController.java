ae75f9baea5172cbe6daef068e58b1d257535eae
Vicente Quiles Sáez <vicenteqsdam@gmail.com>
2018-04-27 21:21:19 +0200

    a

--- src/main/java/es/vqs/memorial/WebSocketController.java
+++ src/main/java/es/vqs/memorial/WebSocketController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
+@CrossOrigin("*")
 public class WebSocketController {
 
 	@Autowired
