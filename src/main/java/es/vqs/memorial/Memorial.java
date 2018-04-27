6dae543b09109a8869bfe719cb7a00d004f30877
Vicente Quiles Sáez <vicenteqsdam@gmail.com>
2018-04-27 21:50:41 +0200

    aaa

--- src/main/java/es/vqs/memorial/Memorial.java
+++ src/main/java/es/vqs/memorial/Memorial.java
@@ -2,12 +2,22 @@ package es.vqs.memorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class Memorial {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Memorial.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Memorial.class, args);
+    }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
 }
