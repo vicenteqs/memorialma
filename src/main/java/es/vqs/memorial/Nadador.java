5e3ffb162e283c89990c9a089a27e997a628f2d4
Vicente Quiles Sáez <vicenteqsdam@gmail.com>
2018-05-02 18:44:30 +0200

    A

--- src/main/java/es/vqs/memorial/Nadador.java
+++ src/main/java/es/vqs/memorial/Nadador.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Getter;
@@ -16,7 +17,7 @@ public class Nadador implements Serializable {
 	private static final long serialVersionUID = -2216382168252563212L;
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private Integer serie;
 	private Integer calle;
